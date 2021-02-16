78c4fd8edf761541f1b794f5e2f41e01546e1f3e
Arushi789 <63102065+Arushi789@users.noreply.github.com>
2021-02-16 16:59:57 +0530

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,8 @@
+pocketMoney=int(input("Enter the pocket money you get"))
+if (pocketMoney>100):
+    print("You are lucky!")
+elif(pocketMoney<10):
+    print("Go on for yourself")
+else:
+    print("You are an average kid..")
+#print(pocketMoney)
